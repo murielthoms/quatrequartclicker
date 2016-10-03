a07f57bb1b03c12722fce34a8586673b2e2421bf
muriel <murielthoms@gmail.com>
2016-10-03 16:42:22 +0200

    exo quatre quart clicker

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
 	//votre code
+	var tab = ["Presque", "bingo", "youpi"]
+	var gateau = 0;
+	
+	$("img").click(function(){
 
+		var random = Math.random()*(tab.length);
+	    var floor = Math.floor(random);
+		gateau++;
+
+		$(".counter").text(gateau);
+
+		if (gateau % 5 === 0) {
+			alert(tab[floor]);
+		};
+		
+		
+
+		
+		
+	});
 
 });
+
+
+
+
+
+
+
+
+
+
+
